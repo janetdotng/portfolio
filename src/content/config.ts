f7c7cfdc56188186ec5be9bf364968d44e909a73
G-mi Kwok <gmik@G-miBook-2.local>
2026-02-21 01:52:30 +0800

    update

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,21 @@
 import { defineCollection, z } from 'astro:content';
-const blogCollection =  defineCollection({
-		type: 'content',
-		schema: z.object({
-			title: z.string(),
-			description: z.string().optional(),
-			publishDate: z.coerce.date(),
-			read: z.number().optional(),
-			tags: z.array(z.string()).optional(),
-			img: z.string().optional(),
-			img_alt: z.string().optional(),
-		}),
+
+const showcase = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    publishDate: z.coerce.date(),
+    read: z.number().optional(),
+
+    cover: z.string(),                // required for grid
+    category: z.array(z.string()),    // for filtering
+    tag: z.string().optional(),       // UI label
+    featured: z.boolean().optional(), // future use
+    externalUrl: z.string().optional()
+  }),
 });
 
 export const collections = {
-  'showcase': blogCollection,
+  showcase
 };
